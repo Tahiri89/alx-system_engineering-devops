a1f75016867280f0e6e7235372d71fe1a831bd8d
abdelkhalk Tahiri <abdelkhalk.tahiri@gmail.com>
2023-11-27 07:26:48 -0800

    0 task correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #create a file in /tmp.
 
 file { 'school':
-     ensure  =>'present',
-     mode    =>'0744',
-     owner   =>'www-data',
-     group   =>'www-data'
-     content =>'I love Puppet',
-     path    =>'/tmp/school'
-     }
+  ensure  =>'present',
+    mode    =>'0744',
+      owner   =>'www-data',
+        group   =>'www-data',
+	  content =>'I love Puppet',
+	    path    =>'/tmp/school',
+	    }
